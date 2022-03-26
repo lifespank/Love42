f0dc8833fc71d8d0fbd15d11d93d49bdd72fad36
K007_김수명 <admin@sumyeong.kim>
2022-03-26 22:27:43 +0900

    [FIX] ViewModel에서 Access Token: String으로 받기

--- app/src/main/java/com/mylittleproject/love42/repository/SetProfileRepository.kt
+++ app/src/main/java/com/mylittleproject/love42/repository/SetProfileRepository.kt
@@ -1,8 +1,6 @@
 package com.mylittleproject.love42.repository
 
-import com.mylittleproject.love42.data.AccessToken
-
 interface SetProfileRepository {
 
-    suspend fun fetchAccessToken(code: String): AccessToken?
+    suspend fun fetchAccessToken(code: String): String?
 }
--- app/src/main/java/com/mylittleproject/love42/repository/SetProfileRepositoryImpl.kt
+++ app/src/main/java/com/mylittleproject/love42/repository/SetProfileRepositoryImpl.kt
@@ -1,17 +1,16 @@
 package com.mylittleproject.love42.repository
 
 import android.util.Log
-import com.mylittleproject.love42.data.AccessToken
 import com.mylittleproject.love42.model.DataSource
 import com.mylittleproject.love42.tools.NAME_TAG
 
 class SetProfileRepositoryImpl(private val dataSource: DataSource.RemoteDataSource) :
     SetProfileRepository {
 
-    override suspend fun fetchAccessToken(code: String): AccessToken? {
+    override suspend fun fetchAccessToken(code: String): String? {
         val token = dataSource.fetchAccessToken(code)
         token.onSuccess {
-            return it
+            return it.accessToken
         }
         token.onFailure {
             Log.e(NAME_TAG, "Auth token fetch failure", it)
