c2137ce753d9df91482ca3921ea5f661b5594cc4
K007_김수명 <admin@sumyeong.kim>
2022-03-29 00:33:53 +0900

    [FEAT] 인트라에서 이미지 불러오기

--- app/src/main/java/com/mylittleproject/love42/tools/BindingAdapter.kt
+++ app/src/main/java/com/mylittleproject/love42/tools/BindingAdapter.kt
@@ -1,10 +1,22 @@
 package com.mylittleproject.love42.tools
 
 import android.view.View
+import android.widget.ImageView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.mylittleproject.love42.R
 
 @BindingAdapter("isVisible")
 fun setVisible(view: View, isVisible: Boolean) {
     view.isVisible = isVisible
+}
+
+@BindingAdapter("imageURL")
+fun setImageWithUrl(view: ImageView, imageURL: String?) {
+    Glide.with(view)
+        .load(imageURL)
+        .circleCrop()
+        .placeholder(R.drawable.ic_baseline_person_24)
+        .into(view)
 }
