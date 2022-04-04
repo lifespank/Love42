34fa481878934242a5cbc9e2d82ab05435bf4b9a
K007_김수명 <admin@sumyeong.kim>
2022-04-04 17:48:43 +0900

    [FEAT] 후보 카드 구체화

--- app/src/main/java/com/mylittleproject/love42/ui/UserInfoListAdapter.kt
+++ app/src/main/java/com/mylittleproject/love42/ui/UserInfoListAdapter.kt
@@ -2,10 +2,14 @@ package com.mylittleproject.love42.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.material.chip.Chip
+import com.mylittleproject.love42.R
 import com.mylittleproject.love42.data.DetailedUserInfo
 import com.mylittleproject.love42.databinding.CardItemBinding
 
@@ -38,6 +42,21 @@ class UserInfoListAdapter :
         fun bind(item: DetailedUserInfo) {
             binding.userInfo = item
             binding.ivProfile.clipToOutline = true
+            item.languages.forEach {
+                binding.cgLanguages.addView(
+                    Chip(binding.root.context)
+                        .apply {
+                            text = it
+                            textSize = 23f
+                            setTextColor(
+                                ContextCompat.getColor(
+                                    binding.root.context,
+                                    R.color.white
+                                )
+                            )
+                            setChipBackgroundColorResource(R.color.purple_200)
+                        })
+            }
         }
 
         fun recycle() {
